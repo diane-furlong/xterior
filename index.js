66340eb4bd72bc8845f3e8677412d17adca2d95e
Diane Furlong <dfurlongm@gmail.com>
2021-02-06 14:08:50 -0500

    css link

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const express = require("express")
 const path = require("path")
 
 //set up express app
-
+var app = express()
 const PORT = process.env.PORT || 5001
 
 //set up express app to handle data parsing
-express()
-    .use(express.static(path.join(__dirname, 'index.html')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 //data
 
 
 //Routes
 //basic route that sends user first to the AJAX page
-    .get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 
 //Starts the server to begin listening
-    .listen(PORT, function(){
+.listen(PORT, function(){
     console.log("App listening on PORT " + PORT)
-    })
+})
