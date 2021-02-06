29a5f4b9e34b92bc82ce01e87edf5b5781d4e260
Diane Furlong <dfurlongm@gmail.com>
2021-02-06 13:03:45 -0500

    added listener

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ app.use(express.json())
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+//Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT)
+})
