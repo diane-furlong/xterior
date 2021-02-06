627d977733c5874783d56dfc50da5badd0f88e63
Diane Furlong <dfurlongm@gmail.com>
2021-02-06 13:35:27 -0500

    maybe this works

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 //dependencies
-// const express = require("express")
+const express = require("express")
 const path = require("path")
 
 //set up express app
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
-// //set up express app to handle data parsing
-// express()
-//     .use(express.static(path.join(__dirname, 'index.html')))
-//     .set('views', path.join(__dirname, 'views'))
-//     .set('view engine', 'ejs')
-//     .get('/', (req, res) => res.render('index.html'))
+//set up express app to handle data parsing
+express()
+    .use(express.static(path.join(__dirname, 'index.html')))
 
 //data
 
 
 //Routes
 //basic route that sends user first to the AJAX page
-app.get('/', function(req, res){
+    .get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 //Starts the server to begin listening
-app.listen(PORT, function(){
+    .listen(PORT, function(){
     console.log("App listening on PORT " + PORT)
-})
+    })
