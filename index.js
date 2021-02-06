23576e999415ae6338aa95ecadfb7c6c2a8309bc
Diane Furlong <dfurlongm@gmail.com>
2021-02-06 13:24:02 -0500

    working with express

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require("express")
 const path = require("path")
 
 //set up express app
-const app = express()
+
 const PORT = process.env.PORT || 5000
 
 //set up express app to handle data parsing
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('index.html'))
 
 //data
 
