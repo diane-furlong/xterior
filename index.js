32327ff19106e4cb97744833e550540bdffb22a5
Diane Furlong <dfurlongm@gmail.com>
2021-02-06 12:59:28 -0500

    added index.js content, port info, etc

---
+++ index.js
@@ -0,0 +1,20 @@
+//dependencies
+const express = require("express")
+var path = require("path")
+
+//set up express app
+const app = express()
+var PORT = process.env.PORT || 5000
+
+//set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//data
+
+
+//Routes
+//basic route that sends user first to the AJAX page
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
